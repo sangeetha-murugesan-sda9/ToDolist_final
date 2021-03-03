fc16c22b34e8164059d138d5e312648c5d269ee5
sangeetha-murugesan-sda9 <geethastockholm@gmail.com>
2021-03-03 19:18:13 +0100

    trying to refactor the github repo

--- README.md
+++
@@ -1,4 +0,0 @@
-# ToDolist_final
-
-Modified with Read as Object and Write as Object.
-
--- TitleComparable.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class TitleComparable implements Comparator<TodoItem> {
-
-    @Override
-    public int compare(TodoItem o1, TodoItem o2) {
-        return o1.getTitle().compareTo(o2.getTitle());
-    }
-}
